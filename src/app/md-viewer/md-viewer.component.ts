3c321e3fd402b425df95924b970eb6d94c7bed73
Sarthak Sharma <7lovesharma7@gmail.com>
2019-04-05 11:50:35 +0530

    Dynamic Value of Readme From Parent

--- src/app/md-viewer/md-viewer.component.ts
+++ src/app/md-viewer/md-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'md-viewer',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./md-viewer.component.css']
 })
 export class MdViewerComponent implements OnInit {
+
+  @Input() src: string;
 
   constructor() { }
 
