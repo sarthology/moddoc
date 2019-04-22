db6e28300753d5ca29b9b98e116037a288be8c7d
Sarthak Sharma <7lovesharma7@gmail.com>
2019-04-22 20:09:14 +0530

    Bug🐞: External links fixed

--- src/app/md-viewer/md-viewer.component.ts
+++ src/app/md-viewer/md-viewer.component.ts
@@ -1,41 +1,62 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { MarkdownService } from 'ngx-markdown';
+import { Component, OnInit, Input } from "@angular/core";
+import { MarkdownService } from "ngx-markdown";
 
 @Component({
-  selector: 'md-viewer',
-  templateUrl: './md-viewer.component.html',
-  styleUrls: ['./md-viewer.component.css']
+  selector: "md-viewer",
+  templateUrl: "./md-viewer.component.html",
+  styleUrls: ["./md-viewer.component.css"]
 })
 export class MdViewerComponent implements OnInit {
-
   @Input() src: string;
+  public externalLink: string;
 
   constructor(private markdownService: MarkdownService) { }
 
   ngOnInit() {
     this.markdownService.renderer.heading = (text: string, level: number) => {
-      if(text.indexOf(":")===text.length-1) text = text.replace(':','');
-      const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
-      return '<h' + level + ' id='+escapedText+'>'
-              + text +
-             '</h' + level + '>';
+      if (text.indexOf(":") === text.length - 1) text = text.replace(":", "");
+      const escapedText = text.toLowerCase().replace(/[^\w]+/g, "-");
+      return (
+        "<h" + level + " id=" + escapedText + ">" + text + "</h" + level + ">"
+      );
     };
   }
 
-  onLoad(e){
-    document.querySelectorAll("a").forEach((e)=>{
-      e.onclick = (event)=>{
-        if(e.getAttribute("href").indexOf("#")===0){
-          event.preventDefault();
-          var top = document.getElementById(e.getAttribute("href").split('#')[1]).offsetTop;
+  onLoad(e) {
+    document.querySelectorAll("a").forEach(e => {
+      let href = e.getAttribute("href");
+      e.onclick = event => {
+        event.preventDefault();
+        if (href.indexOf("#") === 0) {
+          var top = document.getElementById(
+            href.split("#")[1]
+          ).offsetTop;
           window.scrollTo(0, top);
         }
-        else{
-          event.preventDefault();
+        // else if(href.indexOf("md") === href.length - 2){
+        //   this.src = href
+        // }
+        else {
+          document
+            .querySelector(".markdown-body")
+            .setAttribute("style", "display:none");
+          document
+            .querySelector(".browser-body")
+            .setAttribute("style", "display:block");
+          document
+            .querySelector("iframe")
+            .setAttribute("src", href);
         }
-
-      }
-    })
+      };
+    });
   }
 
+  reset() {
+    document
+      .querySelector(".markdown-body")
+      .setAttribute("style", "display:block");
+    document
+      .querySelector(".browser-body")
+      .setAttribute("style", "display:none");
+  }
 }
