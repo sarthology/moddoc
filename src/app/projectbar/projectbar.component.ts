ee1292b78e8407f749b041717c2b2120ca6e022b
Sarthak Sharma <7lovesharma7@gmail.com>
2019-04-26 18:10:08 +0530

    Bug🐞: highlight selected project

--- src/app/projectbar/projectbar.component.ts
+++ src/app/projectbar/projectbar.component.ts
@@ -9,9 +9,9 @@ import { FileService } from "../file.service";
 export class ProjectbarComponent implements OnInit {
 
   public projects:Array<object> = this.fileService.projects;
+  public clicked:any = null;
 
   @Output() currentProject = new EventEmitter<object>();
-
   constructor(private fileService: FileService) {
 
   }
@@ -25,10 +25,27 @@ export class ProjectbarComponent implements OnInit {
   addProject(){
     this.fileService.getFiles().then((project:Object)=>{
       this.currentProject.emit(project);
-    });
+    })
   }
 
   setCurrentProject(project,event){
+    this.clicked = event
     this.currentProject.emit(project);
+  }
+  highlightProject(){
+    if(document.querySelector('.active-project')){
+      document.querySelector('.active-project').classList.remove('active-project')
+    }
+    if( this.clicked === null){
+      console.log("bro");
+      console.log(this.clicked)
+      document.getElementById('project-names').lastElementChild.classList.add('active-project')
+    }
+    else if(this.clicked) {
+      console.log("no");
+      this.clicked.target.closest('.project').classList.add('active-project')
+      this.clicked = null;
+    }
+
   }
 }
