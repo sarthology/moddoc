d4dee3b2d3bd33e92054f0cf724c929eaf36db46
Sarthak Sharma <7lovesharma7@gmail.com>
2019-04-28 16:20:30 +0530

    ng build problem

--- src/app/file.service.ts
+++ src/app/file.service.ts
@@ -1,30 +1,21 @@
 import { Injectable } from '@angular/core'
-import { IpcRenderer,dialog } from 'electron'
+import { ElectronService } from 'ngx-electron';
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class FileService {
-  private ipc: IpcRenderer
   public projects: Array<{file:string,data:Object}> = []
   public currentModule:string;
 
-  constructor() {
-    if ((<any>window).require) {
-      try {
-        this.ipc = (<any>window).require('electron').ipcRenderer
-      } catch (error) {
-        throw error
-      }
-    } else {
-      console.warn('Could not load electron ipc')
-    }
+  constructor(private _electronService: ElectronService) {
+
   }
 
   async getFiles() {
     return new Promise<string[]>((resolve, reject) => {
-      this.ipc.once("getFilesResponse", (event, arg) => {
+      this._electronService.ipcRenderer.once("getFilesResponse", (event, arg) => {
         if(arg && this.distinct(this.projects,arg)){
             this.projects.push(arg)
             resolve(arg)
@@ -33,23 +24,23 @@ export class FileService {
           reject()
         }
       });
-      this.ipc.send("getFiles");
+      this._electronService.ipcRenderer.send("getFiles");
     });
   }
 
   copyCode(data){
-    return this.ipc.send('copyCode',data)
+    return this._electronService.ipcRenderer.send('copyCode',data)
   }
 
   noFileError(error){
-    return this.ipc.send('onFileError',error)
+    return this._electronService.ipcRenderer.send('onFileError',error)
   }
   distinct(projects:any,arg){
     if(projects.filter(project=>project.file!==arg.file).length > 0 || projects.length === 0){
       return true
     }
     else{
-      this.ipc.send('onAlreadyExist','project')
+      this._electronService.ipcRenderer.send('onAlreadyExist','project')
       return false
     }
   }
